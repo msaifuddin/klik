9fab4030f5dc06bcf3e67df1ab49a8ba075f0f38
mss <1064775+msaifuddin@users.noreply.github.com>
2024-06-05 16:50:49 +1000

    Add files via upload

--- c#/Klik/Form1.Designer.cs
+++ c#/Klik/Form1.Designer.cs
@@ -20,14 +20,15 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             label1 = new Label();
             intervalTextBox = new TextBox();
-            start = new Button();
-            stop = new Button();
+            startButton = new Button();
+            stopButton = new Button();
+            statusLabel = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(13, 20);
+            label1.Location = new Point(17, 25);
             label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
             label1.Size = new Size(154, 25);
@@ -36,42 +37,55 @@
             // 
             // intervalTextBox
             // 
-            intervalTextBox.Location = new Point(175, 20);
+            intervalTextBox.Location = new Point(191, 20);
             intervalTextBox.Margin = new Padding(4, 5, 4, 5);
+            intervalTextBox.MaxLength = 4;
             intervalTextBox.Name = "intervalTextBox";
-            intervalTextBox.Size = new Size(74, 31);
+            intervalTextBox.Size = new Size(70, 31);
             intervalTextBox.TabIndex = 1;
             intervalTextBox.Text = "30";
             // 
-            // start
+            // startButton
             // 
-            start.Location = new Point(13, 64);
-            start.Margin = new Padding(4, 5, 4, 5);
-            start.Name = "start";
-            start.Size = new Size(107, 38);
-            start.TabIndex = 2;
-            start.Text = "Start";
-            start.UseVisualStyleBackColor = true;
-            start.Click += startButton_Click;
+            startButton.Location = new Point(17, 83);
+            startButton.Margin = new Padding(4, 5, 4, 5);
+            startButton.Name = "startButton";
+            startButton.Size = new Size(107, 38);
+            startButton.TabIndex = 2;
+            startButton.Text = "Start";
+            startButton.UseVisualStyleBackColor = true;
+            startButton.Click += startButton_Click;
             // 
-            // stop
+            // stopButton
             // 
-            stop.Location = new Point(142, 64);
-            stop.Margin = new Padding(4, 5, 4, 5);
-            stop.Name = "stop";
-            stop.Size = new Size(107, 38);
-            stop.TabIndex = 3;
-            stop.Text = "Stop";
-            stop.UseVisualStyleBackColor = true;
-            stop.Click += stopButton_Click;
+            stopButton.Location = new Point(156, 83);
+            stopButton.Margin = new Padding(4, 5, 4, 5);
+            stopButton.Name = "stopButton";
+            stopButton.Size = new Size(107, 38);
+            stopButton.TabIndex = 3;
+            stopButton.Text = "Stop";
+            stopButton.UseVisualStyleBackColor = true;
+            stopButton.Click += stopButton_Click;
+            // 
+            // statusLabel
+            // 
+            statusLabel.AutoSize = true;
+            statusLabel.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            statusLabel.Location = new Point(17, 53);
+            statusLabel.Margin = new Padding(4, 0, 4, 0);
+            statusLabel.Name = "statusLabel";
+            statusLabel.Size = new Size(146, 25);
+            statusLabel.TabIndex = 4;
+            statusLabel.Text = "Status: Stopped";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(262, 118);
-            Controls.Add(stop);
-            Controls.Add(start);
+            ClientSize = new Size(286, 136);
+            Controls.Add(statusLabel);
+            Controls.Add(stopButton);
+            Controls.Add(startButton);
             Controls.Add(intervalTextBox);
             Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.FixedDialog;
@@ -80,7 +94,6 @@
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "Form1";
-            Opacity = 0.7D;
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Klik";
             FormClosed += Form1_FormClosed;
@@ -92,7 +105,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox intervalTextBox;
-        private System.Windows.Forms.Button start;
-        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label statusLabel;  // Add this line
     }
 }
--- c#/Klik/Form1.cs
+++ c#/Klik/Form1.cs
@@ -1,56 +1,105 @@
 using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using System.Runtime.Versioning;
 
 namespace Klik
 {
+    [SupportedOSPlatform("windows6.1")]
     public partial class Form1 : Form
     {
-        private System.Windows.Forms.Timer timer;
-        private int interval = 30000; // Default to 30 seconds
+        private System.Windows.Forms.Timer? countdownTimer;
+        private int interval;
+        private int countdown;
 
         public Form1()
         {
             InitializeComponent();
+            statusLabel.Text = "Status: Stopped";  // Initialize status label
         }
 
         private void startButton_Click(object sender, EventArgs e)
         {
-            int result;
-            if (int.TryParse(intervalTextBox.Text, out result))
+            if (int.TryParse(intervalTextBox.Text, out int result))
             {
-                interval = result * 1000;
-                timer = new System.Windows.Forms.Timer();
-                timer.Interval = interval;
-                timer.Tick += Timer_Tick;
-                timer.Start();
+                if (result <= 0)
+                {
+                    MessageBox.Show("Interval must be greater than 0", "Invalid Interval", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                interval = result;
+                countdown = result;
+
+                if (countdownTimer == null)
+                {
+                    countdownTimer = new System.Windows.Forms.Timer();
+                    countdownTimer.Interval = 1000;  // 1 second
+                    countdownTimer.Tick += CountdownTimer_Tick;
+                }
+
+                countdownTimer.Start();
+                statusLabel.Text = $"Status: Running, Klik in {countdown}s";  // Update status label
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
 
         private void stopButton_Click(object sender, EventArgs e)
         {
-            if (timer != null)
+            if (countdownTimer != null)
             {
-                timer.Stop();
-                timer.Dispose();
-                timer = null;
+                countdownTimer.Stop();
+                countdownTimer.Dispose();
+                countdownTimer = null;
             }
+
+            statusLabel.Text = "Status: Stopped";  // Update status label
         }
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
-            if (timer != null)
+            if (countdownTimer != null)
             {
-                timer.Stop();
-                timer.Dispose();
-                timer = null;
+                countdownTimer.Stop();
+                countdownTimer.Dispose();
+                countdownTimer = null;
             }
+
             Application.Exit();
         }
 
-        private void Timer_Tick(object sender, EventArgs e)
+        private void CountdownTimer_Tick(object? sender, EventArgs e)
         {
-            var pos = Cursor.Position;
-            MouseOperations.LeftClick(pos.X, pos.Y);
+            countdown--;
+            if (countdown > 0)
+            {
+                statusLabel.Text = $"Status: Running, Klik in {countdown}s";
+            }
+            else
+            {
+                var pos = Cursor.Position;
+                MouseOperations.LeftClick(pos.X, pos.Y);
+                statusLabel.Text = "Status: Running, Klik!";
+                countdown = interval;
+            }
+        }
+    }
+
+    [SupportedOSPlatform("windows6.1")]
+    public static class MouseOperations
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        public static void LeftClick(int x, int y)
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, x, y, 0, 0);
         }
     }
 }
